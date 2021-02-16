25dbc8d9c71aafe7ceef50870c7b59a7338362f0
ohmy04180 <ohmy0418@mobigen.com>
2021-02-16 12:58:10 +0900

    Resolve: vue test

--- folder-structure/src/components/pagination-list.vue
+++ folder-structure/src/components/pagination-list.vue
@@ -15,337 +15,54 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="list in lists" v-bind:key="list.id">
-          <td>{{ list.userId }}</td>
-          <td>{{ list.id }}</td>
-          <td>{{ list.title }}</td>
-          <td>{{ list.body }}</td>
+        <tr v-for="item in listArray" v-bind:key="item.id">
+          <td>{{ item.userId }}</td>
+          <td>{{ item.id }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.body }}</td>
         </tr>
       </tbody>
     </table>
-    <ul class="pagination">
-      <li v-for="(list, index) in lists" v-bind:key="list.id">
-        <button type="button" v-on:click.prevent="doSomething">
-          {{ index + 1 }}
-        </button>
-      </li>
-    </ul>
-    <!-- <button v-on:click="fetchData">데이터 가져오기</button> -->
+    <div class="pagination">
+      <button type="button" class="pagination__button">
+        {{ prevText }}
+      </button>
+      <button type="button" class="pagination__button">
+        {{ nextText }}
+      </button>
+    </div>
+    <button v-on:click="test">데이터 가져오기</button>
   </div>
 </template>
 
 <script>
-export default {
+ export default {
   name: "pagination-list",
   data() {
     return {
       tableTitle: "JSON Data 불러오기",
-      lists: [
-        { userId: 1, id: 1, title: "hoho", body: "hihi" },
-        { userId: 1, id: 2, title: "hoho", body: "hihi" },
-        { userId: 1, id: 3, title: "hoho", body: "hihi" },
-      ],
-      dataArray: [],
+      prevText:"이전으로",
+      nextText:"다음으로",
+      dataArrays: []
     };
+  },
+  props: {
+    listArray: {
+      type: Array,
+      required: true
+    },
+
   },
   methods: {
     doSomething() {
       alert("click");
     },
+    test(){
+      console.log(this.listArray);
+    }
   },
 };
 </script>
 
 <style>
-@charset "UTF-8";
-html,
-body,
-div,
-span,
-object,
-iframe,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-blockquote,
-pre,
-abbr,
-address,
-cite,
-code,
-del,
-dfn,
-em,
-img,
-ins,
-kbd,
-q,
-samp,
-small,
-strong,
-sub,
-sup,
-var,
-b,
-i,
-dl,
-dt,
-dd,
-ol,
-ul,
-li,
-fieldset,
-form,
-label,
-legend,
-table,
-caption,
-tbody,
-tfoot,
-thead,
-tr,
-th,
-td,
-article,
-aside,
-canvas,
-details,
-figcaption,
-figure,
-footer,
-header,
-hgroup,
-menu,
-nav,
-section,
-summary,
-time,
-mark,
-audio,
-video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  vertical-align: baseline;
-}
-
-/* HTML5 display-role reset for older browsers */
-article,
-aside,
-details,
-figcaption,
-figure,
-footer,
-header,
-hgroup,
-menu,
-nav,
-section {
-  display: block;
-}
-
-sup {
-  vertical-align: super;
-}
-
-sub {
-  vertical-align: sub;
-}
-
-ol,
-ul {
-  list-style: none;
-}
-
-blockquote,
-q {
-  quotes: none;
-}
-
-em {
-  font-style: normal;
-}
-
-blockquote:before,
-blockquote:after,
-q:before,
-q:after {
-  content: "";
-  content: none;
-}
-
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-
-a:focus,
-button:focus,
-input:focus {
-  outline: 0;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: "NotoSansKR", "メイリオ", Meiryo, "Yu Gothic UI",
-    "Microsoft YaHei UI", "Microsoft JhengHei UI", -apple-system,
-    BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", sans-serif;
-}
-
-code {
-  font-family: Consolas, Menlo, "SFMono-Regular", Courier, monospace;
-}
-
-strong {
-  font-weight: 700;
-}
-
-.hidden {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border: 0;
-  visibility: visible;
-  white-space: nowrap;
-}
-
-.ir-pm {
-  /* 레이아웃에 영향을 끼치지 않도록 */
-  position: absolute;
-  /* 스크린 리더가 읽을 수 있도록 */
-  width: 1px;
-  height: 1px;
-  /* 눈에 보이는 부분을 제거 */
-  clip: rect(0 0 0 0);
-  margin: -1px;
-  overflow: hidden;
-}
-
-.img-g {
-  display: block;
-  width: 100%;
-}
-
-.caption-out {
-  width: 0;
-  height: 0;
-  padding: 0;
-  margin: 0;
-  visibility: hidden;
-}
-
-.filter-highlight {
-  background-color: #fff09c;
-}
-
-body {
-  width: 1180px;
-  margin: 50px auto;
-}
-
-.table {
-  width: 400px;
-  margin: 0 auto;
-  border: 1px solid #ccc;
-}
-
-.table__caption {
-  margin-bottom: 8px;
-  font-size: 16px;
-  font-weight: 400;
-  text-align: center;
-  color: #666;
-}
-
-.table table {
-  width: 100%;
-  border: 1px solid #ccc;
-}
-
-.table th,
-.table td {
-  padding: 4px;
-  vertical-align: middle;
-  word-break: break-all;
-}
-
-.table th {
-  font-size: 15px;
-  font-weight: 500;
-  border-bottom: 1px solid #ccc;
-}
-
-.table td {
-  font-size: 14px;
-  font-weight: 400;
-  text-align: left;
-  border-bottom: 1px solid #ccc;
-}
-
-th + th,
-td + td {
-  border-left: 1px solid #ccc;
-}
-
-.table tr:hover td {
-  background-color: #fafafa;
-}
-
-.pagination {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin-top: 16px;
-}
-
-.pagination__list {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-
-.pagination__text {
-  display: block;
-  padding: 0 4px;
-}
-
-.pagination__button {
-  text-decoration: none;
-}
-
-.pagination__button {
-  border: none;
-}
-
-.pagination__button {
-  display: block;
-  height: 30px;
-  line-height: 30px;
-  padding: 0 16px;
-  background-color: transparent;
-  border-radius: 5px;
-  font-size: 16px;
-  text-align: center;
-  cursor: pointer;
-  color: #666;
-}
-.pagination__button:hover {
-  background-color: #fafafa;
-}
-.pagination__button:active {
-  background-color: #f0f0f0;
-}
 </style>
